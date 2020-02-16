955aae238a9ac023b2cd6627cb1103966f2c27fa
Wataru Shito <wataru007jp@gmail.com>
2020-02-16 12:59:41 +0900

    added usage

--- ch01/js/src/GenerateData.js
+++ ch01/js/src/GenerateData.js
@@ -1,7 +1,17 @@
 'use strict';
 
+/**
+ * Usage:
+ *   node GenerateData.js > 7digits.txt
+ *
+ * Generates 1 million records of 7 digits integer string per line.
+ * This is the implementation of Column 1, Problem 4 from Programming
+ * Pearls by John Bentley.
+ */
+
 const d3 = require('d3');
 
+// slice makes a shallow copied array
 const sevenDigitInt = d3.shuffle(d3.range(10 ** 7)).slice(0, 10 ** 6);
 
 sevenDigitInt.map(num => console.log(String(num).padStart(7, "0")));
