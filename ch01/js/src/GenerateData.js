8df03755d9abaec814e35474169704b7f90255b9
Wataru Shito <wataru007jp@gmail.com>
2020-02-16 12:51:44 +0900

    問題に合わせ7桁の数字を100万個に変更

--- ch01/js/src/GenerateData.js
+++ ch01/js/src/GenerateData.js
@@ -2,6 +2,6 @@
 
 const d3 = require('d3');
 
-const sevenDigitInt = d3.shuffle(d3.range(10 ** 7));
+const sevenDigitInt = d3.shuffle(d3.range(10 ** 7)).slice(0, 10 ** 6);
 
 sevenDigitInt.map(num => console.log(String(num).padStart(7, "0")));
