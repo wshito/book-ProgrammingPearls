694d8ad6914794bb4a65f5604a7b44ed9a8f91eb
Wataru Shito <wataru007jp@gmail.com>
2020-02-20 00:18:17 +0900

    bug fix

--- ch01/js/src/SortWithLibrary.js
+++ ch01/js/src/SortWithLibrary.js
@@ -13,7 +13,7 @@ const readline = require('readline');
 
 const filename = "7digits.txt";
 
-function sortWithLibrary(fname) {
+function sortWithLibrary (fname) {
   // regular array holds all the numbers and uses Array.sort()
   const arry = new Array();
 
@@ -25,20 +25,23 @@ function sortWithLibrary(fname) {
   rl.on('line', (line) => {
     arry.push(parseInt(line));
   }).on('close', () => {
-    arry.sort();
-    const ws = fs.createWriteStream(fname.slice(0, -4) + "-lib-sorted.txt");
-    arry.forEach((val, idx) => {
-      if (val) {
-        ws.write(String(idx).padStart(7, "0"));
-        ws.write('\n');
-      }
+    arry.sort((a, b) => {
+      if (a < b) return -1;
+      else if (a > b) return 1;
+      else return 0
     });
+    const ws = fs.createWriteStream(fname.slice(0, -4) + "-lib-sorted.txt");
+    arry.forEach(val => {
+      ws.write(String(val).padStart(7, "0"));
+      ws.write('\n');
+    });
+    ws.end();
     printHeapUsage();
   });
 
 }
 
-function printHeapUsage() {
+function printHeapUsage () {
   const used = process.memoryUsage().heapUsed / 1024 / 1024;
   console.log("Heap used: %d MB", Math.round(used * 100) / 100);
 }
